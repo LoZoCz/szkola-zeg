e73d672e50818a473d7e9c57afb7535234b1320b
LoZo <87641267+LoZoCz@users.noreply.github.com>
2023-02-03 09:08:55 +0100

    Add files via upload

--- czernik-1-02-szymkowiak/rezerwacja.php
+++ czernik-1-02-szymkowiak/rezerwacja.php
@@ -12,6 +12,16 @@
         $ppl = $_POST["ppl"];
         $phone = $_POST["phone"];
 
+        $sql = "INSERT INTO `rezerwacje` (id, nr_stolika, data_rez, liczba_osob, telefon) VALUES (NULL, NULL, '$data', $ppl, '$phone');";
+
+        $sqlQuery = mysqli_query($connect, $sql); 
+
+        if($sqlQuery){
+            echo('Dodano rezerwacje do bazy');
+        } else{
+            echo('Rezerwacja nie powiodła się');
+        }
+
         mysqli_close($connect);
     } else {
         echo('Brak połączenia');
