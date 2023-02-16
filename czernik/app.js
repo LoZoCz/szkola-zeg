c5fac40e0d52c9edbba1f3eadb719eb0891cf389
LoZo <87641267+LoZoCz@users.noreply.github.com>
2023-02-16 22:03:03 +0100

    Add files via upload

--- czernik/app.js
+++ czernik/app.js
@@ -1,3 +1,4 @@
+//Pobranie elementow do edycji
 const btnsA = document.querySelectorAll('.js-btn-a');
 const btnsZ = document.querySelectorAll('.js-btn-z');
 // console.log(btns);
@@ -9,40 +10,45 @@ const nazwa = document.querySelectorAll('.nazwa');
 
 const rows = document.querySelectorAll('.row1');
 
-for(let i = 0; i < ilosc.length; i++){
-    const valueIle = ilosc[i].innerHTML;
 
-    if(valueIle == 0){
-        ilosc[i].style.backgroundColor = 'red';
-    }else if(valueIle > 1 && valueIle < 5){
-        ilosc[i].style.backgroundColor = 'yellow';
-    }else if(valueIle> 5 || nowaIlosc < 5){
-        ilosc[i].style.backgroundColor = 'honeydew';
+//Funkcja sprawdzajaca wartosc komorki w tabeli
+function check(){
+    for(let i = 0; i < ilosc.length; i++){
+        const valueIle = ilosc[i].innerHTML;
+    
+        if(valueIle == 0){
+            ilosc[i].style.backgroundColor = 'red';
+        }else if(valueIle > 1 && valueIle < 5){
+            ilosc[i].style.backgroundColor = 'yellow';
+        }else if(valueIle >= 5 || nowaIlosc >= 5){
+            ilosc[i].style.backgroundColor = 'honeydew';
+        }
     }
 }
 
+//Wywolanie funkcji
+check();
 
+
+//Przycik zmieniajacy wartosc w tabeli i wywolanie funkcji sprawdzajacej
 for(let a = 0; a<btnsA.length; a++){
     btnsA[a].addEventListener('click', () => {
         // console.log('dziala');
-
         let nowaIlosc = prompt('Podaj nową ilość:');
-        // console.log(nowaIlosc);
 
-        const valueIle = ilosc[a].innerHTML;
-        
-        if(nowaIlosc == 0){
-            ilosc[a].style.backgroundColor = 'red';
-        }else if(nowaIlosc > 1 && nowaIlosc < 5){
-            ilosc[a].style.backgroundColor = 'yellow';
-        }else if(nowaIlosc < 5){
-            ilosc[a].style.backgroundColor = 'honeydew';
+        if(isNaN(nowaIlosc) || nowaIlosc == null || nowaIlosc == ""){
+            alert('Wprawadź wartość liczbową');
+        }else{
+            ilosc[a].innerHTML = nowaIlosc;
         }
-
-        ilosc[a].innerHTML = nowaIlosc;
+        // console.log(nowaIlosc);
+        
+        check();
     });
 }
 
+
+// Przycisk umozliwiajacy zoabczyc czy zamowienia zostalo zlozone i jego numer
 let num = 0;
 
 for(let b = 0; b<btnsZ.length; b++){
